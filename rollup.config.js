509bab4b4bdda3af06c6e231757f59b89e34b079
themountain01 <themountain193@gmail.com>
2024-11-06 19:12:22 +0000

    #1 - Initial commit of skeleton website files

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import copy from 'rollup-plugin-copy';
+import livereload from 'rollup-plugin-livereload';
+import serve from 'rollup-plugin-serve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+
+export default {
+    input: 'src/js/index.js',
+    output: {
+        file: 'dist/bundle.js',
+        format: 'iife',
+        sourcemap: true,
+    },
+    plugins: [
+        nodeResolve(), 
+
+        postcss({
+            extract: 'bundle-min.css',
+            minimize: true,
+        }),
+
+        copy({
+            targets: [
+                { src: 'public/index.html', dest: 'dist' },
+                { src: 'src/pages/*.html', dest: 'dist' },
+                { src: 'public/images/**/*', dest: 'dist/images' },
+            ],
+            overwrite: true,
+            hook: 'buildStart',
+            verbose: true,
+        }),
+
+        serve({
+            open: true, 
+            contentBase: 'dist', 
+            port: 3000,
+        }),
+
+        livereload({
+            watch: ['dist', 'public'],
+            delay: 2,
+            verbose: true,
+        }),
+    ],
+};
